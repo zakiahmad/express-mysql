3e582432e41d7e3fd6a105d0719c00be91f7f304
zakiahmad <ahmadmuzaki@gmail.com>
2022-07-31 15:10:51 +0700

    menambahkan query ke db

--- library/database.js
+++ library/database.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 let connection = mysql.createConnection({
    host:        'localhost',
    user:        'root',
-   password:    '',
+   password:    'tidakada',
    database:    'db_express_mysql'
  });
 connection.connect(function(error){
